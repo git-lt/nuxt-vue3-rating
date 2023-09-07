f2f6bdaa7ceb2b63ee6152b89f044e804c89b1dc
liutaotaom <hz_liutao@qq.com>
2023-09-07 23:10:53 +0800

    feat: mflow-border

---
+++ pages/css/mflow-border.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="w-full h-full bg-black flex justify-center items-center">
+    <div class="mflow-box flex justify-center items-center overflow-hidden w-52 h-32 rounded-3xl relative before:w-[calc(100%-6px)] before:h-[calc(100%-6px)] before:bg-black before:rounded-3xl before:absolute before:top-[3px] before:left-[3px] before:z-10 after:absolute after:w-[300%] after:h-[300%] after:rounded-3xl  after:z-0 after:top-[-100%] after:left-[-100%]">
+      <div class="text-orange-400 text-3xl z-20 absolute">
+        流光边框
+      </div>
+    </div>
+    <div class="w-8" />
+    <div class="mflow-box2 flex justify-center items-center overflow-hidden w-52 h-32 rounded-3xl relative before:w-[calc(100%-6px)] before:h-[calc(100%-6px)] before:bg-black before:rounded-3xl before:absolute before:top-[3px] before:left-[3px] before:z-10 after:absolute after:w-[300%] after:h-[300%] after:rounded-3xl  after:z-0 after:top-[-100%] after:left-[-100%]">
+      <div class="text-orange-400 text-3xl z-20 absolute">
+        流光边框
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+.mflow-box::after {
+  animation: rotate 5s linear infinite;
+  background-image: conic-gradient(#eeeeee22, #f0c237, #eeeeee22 30%);
+}
+
+.mflow-box2::after {
+  animation: rotate 5s linear infinite;
+  background-image: conic-gradient(#eeeeee22, #fc97e9, #e7d0fe, #cca9fd, #02eded, #adf674, #fefe05, #fec900, #eeeeee22 60%);
+}
+
+@keyframes rotate {
+    to {
+      transform: rotate(360deg);
+    }
+}
+</style>
---
+++ pages/css/text-underline.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="bg-white w-full h-full flex justify-center items-center">
+    <div class="w-96">
+      <span class="text">这里是一个很有意思的下划线过渡效果CSS演示</span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.text {
+  cursor: pointer;
+  line-height: 2;
+  font-size: 32px;
+  background: linear-gradient(to right, #82b7a3, #67a7d1) no-repeat right bottom;
+  background-size: 0 2px;
+  transition: background-size .6s;
+  &:hover {
+    background-position-x: left;
+    background-size: 100% 2px;
+  }
+}
+</style>
